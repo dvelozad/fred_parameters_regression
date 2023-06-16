c8200045471fc4f1950ce6484b6ab37b9230830e
Diego Veloza Diaz <dvelozad@unal.edu.co>
2023-06-15 22:13:51 -0500

    feat: Added base scripts

---
+++ scripts/variable_assignation.py
@@ -0,0 +1,26 @@
+dic_var = {'alpha':1,'gamma':2,'kappa':3,'delta':4}
+
+variables_vars = {
+        'alpha': ['variantalpha_transmissibility',
+                'variantalpha_transmissibility_factor',
+                'variantalpha_imports_factor',
+                'variantalpha_cross_protection_prob'],
+
+        'gamma': ['variantgamma_transmissibility',
+                'variantgamma_transmissibility_factor',
+                'variantgamma_imports_factor',
+                'variantgamma_cross_protection_prob',
+                'variantgamma_severity_factor'],
+
+        'kappa': ['variantkappa_transmissibility',
+                'variantkappa_transmissibility_factor',
+                'variantkappa_cross_protection_prob',
+                'variantkappa_introduction_day',
+                'variantkappa_severity_factor'],
+
+        'delta': ['variantdelta_transmissibility',
+                'variantdelta_transmissibility_factor',
+                'variantdelta_imports_factor',
+                'variantdelta_cross_protection_prob',
+                'variantdelta_severity_factor']
+}
